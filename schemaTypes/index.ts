7c4023019a06a05ba8d51857430589b54165ecdc
Arjan Spannenburg <arjanspannenburg@Arjans-MacBook-Pro-2.local>
2026-01-14 21:20:32 +0100

    Add site schema and update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import { type SchemaTypeDefinition } from 'sanity'
+
+// documents
+import site from './documents/site'
+
+// export all schema types
+export const schemaTypes: SchemaTypeDefinition[] = [
+  site,
+]
