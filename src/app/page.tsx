99a81c374f03a07d9eb4fa88362b18b6f8c706f2
spannenburg <arjan@spannenburg.art>
2026-01-14 23:10:53 +0100

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Welkom bij Arjan Spannenburg</h1>
-      <p>De site wordt online gevuld via Sanity Studio.</p>
-    </main>
-  )
-}
