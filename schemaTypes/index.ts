bd3163dc682f2bf20a136a71b9f71a03cfec3392
spannenburg <arjan@spannenburg.art>
2026-01-15 11:10:16 +0100

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,25 +1,39 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+// Basis types
 import { blockContent } from './blockContent'
 import { category } from './category'
 import { author } from './author'
+import { post } from './post'
+
+// Jouw Kunst & Track Record
 import { project } from './project'
 import { artwork } from './artwork'
-import { post } from './post'
-// De nieuwe bestanden importeren:
 import { venue } from './venue'
 import { exhibition } from './exhibition'
 
+// Page Modules (De "Meubels" voor je homepage)
+import { hero } from './hero'
+import { artworkGrid } from './artworkGrid'
+import { postList } from './postList'
+
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
+    // Basis
     blockContent,
     category,
     author,
+    post,
+    
+    // Kunst
     project,
     artwork,
-    post,
-    // De nieuwe bestanden activeren:
     venue,
     exhibition,
+    
+    // Modules
+    hero,
+    artworkGrid,
+    postList, // <--- NIEUW
   ],
 }
