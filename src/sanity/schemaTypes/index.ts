bb9153b01e58d9c48741484dc6da85e5e31cc31b
spannenburg <arjan@spannenburg.art>
2026-01-15 21:38:19 +0100

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,54 +1,48 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 // ---------------------------------------------
-// 1. BESTAANDE BESTANDEN (Die los in de map staan)
+// 1. BESTAANDE BESTANDEN (Met accolades { })
 // ---------------------------------------------
-import page from './page'
-import post from './post'
-import artwork from './artwork'
-import project from './project'
-import venue from './venue'
-import exhibition from './exhibition'
-import author from './author'
-import category from './category'
-import blockContent from './blockContent'
-import metadata from './metadata'
-
-// Oude modules die waarschijnlijk ook los staan:
-import artworkGrid from './artworkGrid' 
-import postList from './postList' 
+import { page } from './page'
+import { post } from './post'
+import { artwork } from './artwork'
+import { project } from './project'
+import { venue } from './venue'
+import { exhibition } from './exhibition'
+import { author } from './author'
+import { category } from './category'
+import { blockContent } from './blockContent'
+import { metadata } from './metadata'
+import { artworkGrid } from './artworkGrid'
+import { postList } from './postList'
 
 // ---------------------------------------------
-// 2. NIEUWE MODULES (In je nieuwe mapje)
+// 2. NIEUWE MODULES (Zonder accolades, want export default)
 // ---------------------------------------------
-// We pakken specifiek de nieuwe versie die we net gemaakt hebben
-import hero from './modules/hero' 
-
+import hero from './modules/hero'
 
 // ---------------------------------------------
 // 3. DE LIJST SAMENVOEGEN
 // ---------------------------------------------
 export const schemaTypes: SchemaTypeDefinition[] = [
-  // Documenten (Pagina's & Posts)
+  // Documenten
   page,
   post,
-  
-  // Jouw Kunst & Werk (Ja, we zetten ze gewoon aan!)
+
+  // Kunst & Projecten
   artwork,
   project,
   venue,
   exhibition,
-  
-  // Taxonomie (Categorieën & Auteurs)
-  category,
-  author,
 
-  // Hulpstukken
+  // Overige
+  author,
+  category,
   blockContent,
   metadata,
 
-  // Modules (Blokken voor op de pagina)
-  hero,         // <--- Dit is de belangrijkste voor nu!
+  // Modules
+  hero,
   artworkGrid,
   postList,
 ]
