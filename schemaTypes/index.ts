5e68b4340ea72010848017b53d6dedec44c1ccf6
spannenburg <arjan@spannenburg.art>
2026-01-15 10:04:12 +0100

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,11 +2,18 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import { blockContent } from './blockContent'
 import { category } from './category'
-import { post } from './post'
 import { author } from './author'
-import { project } from './project' // Dit is je "Series" type
-import { artwork } from './artwork' // <-- Deze moet erbij staan!
+import { project } from './project'
+import { artwork } from './artwork'
+import { post } from './post'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContent, category, post, author, project, artwork],
+  types: [
+    blockContent,
+    category,
+    author,
+    project,
+    artwork,
+    post,
+  ],
 }
