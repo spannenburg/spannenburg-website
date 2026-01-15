ce100033a1688b9fc9727dff7746e8be749c1944
spannenburg <arjan@spannenburg.art>
2026-01-15 16:47:49 +0100

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,187 +1,65 @@
-import { fetchSanityLive } from './fetch'
-import { groq } from 'next-sanity'
-import errors from '@/lib/errors'
-import { BLOG_DIR } from '@/lib/env'
+import { defineQuery } from 'next-sanity'
+import { sanityFetch } from './live'
 
-export const LINK_QUERY = groq`
-	...,
-	internal->{
-		_type,
-		title,
-		metadata
-	}
-`
+// 1. De functie om gewone pagina's op te halen (Home, Over ons, etc.)
+export const getPage = async ({ slug }: { slug?: string[] }) => {
+  // Als er geen slug is, bedoelen we de homepage ('index')
+  const targetSlug = slug ? slug.join('/') : 'index'
 
-const NAVIGATION_QUERY = groq`
-	title,
-	items[]{
-		${LINK_QUERY},
-		link{ ${LINK_QUERY} },
-		links[]{ ${LINK_QUERY} }
-	}
-`
+  const query = defineQuery(`
+    *[_type == "page" && metadata.slug.current == $targetSlug][0]{
+      _id,
+      _type,
+      title,
+      modules[]{
+        ...,
+        _type == 'hero' => {
+          ...,
+          image {
+            ...,
+            asset->
+          }
+        }
+      },
+      metadata
+    }
+  `)
 
-export const IMAGE_QUERY = groq`
-	...,
-	'lqip': @.asset->metadata.lqip
-`
-
-const ASSET_IMG_QUERY = groq`
-	...,
-	image { ${IMAGE_QUERY} }
-`
-
-export const CTA_QUERY = groq`
-	...,
-	link{ ${LINK_QUERY} }
-`
-
-export const REPUTATION_QUERY = groq`
-	_type == 'reputation-block' => { reputation-> }
-`
-
-export const MODULES_QUERY = groq`
-	...,
-	ctas[]{
-		...,
-		link{ ${LINK_QUERY} }
-	},
-	_type == 'blog-list' => { filteredCategory-> },
-	_type == 'breadcrumbs' => { crumbs[]{ ${LINK_QUERY} } },
-	_type == 'callout' => {
-		content[]{
-			...,
-			${REPUTATION_QUERY}
-		}
-	},
-	_type == 'card-list' => {
-		cards[]{
-			...,
-			ctas[]{ ${CTA_QUERY} }
-		}
-	},
-	_type == 'creative-module' => {
-		modules[]{
-			...,
-			subModules[]{
-				...,
-				ctas[]{ ${CTA_QUERY} }
-			}
-		}
-	},
-	_type == 'hero' => {
-		content[]{
-			...,
-			${REPUTATION_QUERY}
-		},
-		assets[]{
-			...,
-			_type == 'img' => { ${ASSET_IMG_QUERY} }
-		}
-	},
-	_type == 'hero.saas' => {
-		content[]{
-			...,
-			${REPUTATION_QUERY}
-		},
-		assets[]{
-			...,
-			_type == 'img' => { ${ASSET_IMG_QUERY} }
-		}
-	},
-	_type == 'hero.split' => {
-		content[]{
-			...,
-			${REPUTATION_QUERY}
-		},
-		assets[]{
-			...,
-			_type == 'img' => { ${ASSET_IMG_QUERY} }
-		}
-	},
-	_type == 'logo-list' => { logos[]-> },
-	_type == 'person-list' => { people[]-> },
-	_type == 'pricing-list' => {
-		tiers[]->{
-			...,
-			ctas[]{ ${CTA_QUERY} }
-		}
-	},
-	_type == 'richtext-module' => {
-		content[]{
-			...,
-			_type == 'image' => { ${IMAGE_QUERY} }
-		},
-		'headings': select(
-			tableOfContents => content[style in ['h2', 'h3', 'h4', 'h5', 'h6']]{
-				style,
-				'text': pt::text(@)
-			}
-		),
-	},
-	_type == 'tabbed-content' => {
-		tabs[]{
-			...,
-			ctas[]{ ${CTA_QUERY} }
-		}
-	},
-	_type == 'testimonial.featured' => { testimonial-> },
-	_type == 'testimonial-list' => { testimonials[]-> },
-`
-
-export const GLOBAL_MODULE_PATH_QUERY = groq`
-	string::startsWith($slug, path)
-	&& select(
-		defined(excludePaths) => count(excludePaths[string::startsWith($slug, @)]) == 0,
-		true
-	)
-`
-
-export const TRANSLATIONS_QUERY = groq`
-	'translations': *[_type == 'translation.metadata' && references(^._id)].translations[].value->{
-		'slug': metadata.slug.current,
-		language
-	}
-`
-
-export async function getSite() {
-	const site = await fetchSanityLive<Sanity.Site>({
-		query: groq`
-			*[_type == 'site'][0]{
-				...,
-				ctas[]{ ${CTA_QUERY} },
-				headerMenu->{ ${NAVIGATION_QUERY} },
-				footerMenu->{ ${NAVIGATION_QUERY} },
-				social->{ ${NAVIGATION_QUERY} },
-				'ogimage': ogimage.asset->url
-			}
-		`,
-	})
-
-	if (!site) throw new Error(errors.missingSiteSettings)
-
-	return site
+  return await sanityFetch({ query, params: { targetSlug } })
 }
 
-export async function getTranslations() {
-	return await fetchSanityLive<Sanity.Translation[]>({
-		query: groq`*[_type in ['page', 'blog.post'] && defined(language)]{
-			'slug': '/' + select(
-				_type == 'blog.post' => '${BLOG_DIR}/' + metadata.slug.current,
-				metadata.slug.current != 'index' => metadata.slug.current,
-				''
-			),
-			'translations': *[_type == 'translation.metadata' && references(^._id)].translations[].value->{
-				'slug': '/' + select(
-					_type == 'blog.post' => '${BLOG_DIR}/' + language + '/' + metadata.slug.current,
-					metadata.slug.current != 'index' => language + '/' + metadata.slug.current,
-					language
-				),
-				_type == 'blog.post' => {
-					'slugBlogAlt': '/' + language + '/${BLOG_DIR}/' + metadata.slug.current
-				},
-				language
-			}
-		}`,
-	})
+// 2. De functie om blog posts op te halen (DIT MISTE JE)
+export const getPost = async ({ slug }: { slug?: string[] }) => {
+  const targetSlug = slug ? slug.join('/') : ''
+
+  const query = defineQuery(`
+    *[_type == "post" && metadata.slug.current == $targetSlug][0]{
+      _id,
+      _type,
+      title,
+      modules, // Hier halen we de inhoud van de blog op
+      metadata,
+      date,
+      author->{
+        name,
+        image
+      }
+    }
+  `)
+
+  return await sanityFetch({ query, params: { targetSlug } })
+}
+
+// 3. De functie om ALLE posts op te halen (voor lijstjes)
+export const getPosts = async () => {
+  const query = defineQuery(`
+    *[_type == "post"] | order(date desc) {
+      _id,
+      title,
+      metadata,
+      date
+    }
+  `)
+  
+  return await sanityFetch({ query, params: {} })
 }
