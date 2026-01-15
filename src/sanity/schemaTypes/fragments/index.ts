19f0663944d2f938470e0dec37f11beb27ff3165
spannenburg <arjan@spannenburg.art>
2026-01-15 14:12:55 +0100

    Delete src/sanity/schemaTypes/fragments directory

--- src/sanity/schemaTypes/fragments/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as imageBlock } from './image-block'
-export { default as admonition } from './admonition'
-export { default as alignItems } from './alignItems'
-export { default as textAlign } from './textAlign'
