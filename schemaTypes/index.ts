ea48100a69cd61f39d4aaab2676e23cef9b4b03d
spannenburg <arjan@spannenburg.art>
2026-01-15 11:26:59 +0100

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,39 +1,35 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-// Basis types
+// 1. Basis blokken (stonden er al een uur)
 import { blockContent } from './blockContent'
 import { category } from './category'
 import { author } from './author'
 import { post } from './post'
 
-// Jouw Kunst & Track Record
+// 2. Jouw Core Business (Kunst & Track Record)
 import { project } from './project'
 import { artwork } from './artwork'
 import { venue } from './venue'
 import { exhibition } from './exhibition'
 
-// Page Modules (De "Meubels" voor je homepage)
+// 3. De Nieuwe Modules (Die de error veroorzaakten)
 import { hero } from './hero'
 import { artworkGrid } from './artworkGrid'
 import { postList } from './postList'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    // Basis
+    // We voegen ze allemaal toe aan de lijst:
     blockContent,
     category,
     author,
     post,
-    
-    // Kunst
     project,
     artwork,
     venue,
     exhibition,
-    
-    // Modules
     hero,
     artworkGrid,
-    postList, // <--- NIEUW
+    postList,
   ],
 }
