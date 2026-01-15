eafbbf41f1b644ecfd95488a680f7f40ac8408b0
spannenburg <arjan@spannenburg.art>
2026-01-15 14:10:34 +0100

    Create category.ts

---
+++ src/sanity/schemaTypes/category.ts
@@ -0,0 +1,21 @@
+import { defineField, defineType } from 'sanity'
+import { TagIcon } from '@sanity/icons'
+
+export const category = defineType({
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  icon: TagIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+})
